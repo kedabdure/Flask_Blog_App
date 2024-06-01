732d4db84e453daf358ae74905157a9b92f5a8bf
kedabudre <abdurehimk77@gmail.com>
2024-06-01 14:32:08 +0300

    getting started

--- flaskblog.py
+++ flaskblog.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+@app.route("/home")
+def home():
     return "<h1>hello  AbdK </h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About section</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
