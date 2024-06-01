8be6271108546a59e9b48ab0be617b374b99f5e8
kedabudre <abdurehimk77@gmail.com>
2024-06-01 18:09:27 +0300

    create templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '87008de58453f89f1af43456cc8bdf00'
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>hello  AbdK </h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About section</h1>"
+    return render_template('about.html')
+
+# @app.route("/register")
+# def register():
+#     form = RegistrationForm()
+#     return render_template('register.html', title='Register', form=form)
+
+# @app.route("/login")
+# def register():
+#     form = RegistrationForm()
+#     return render_template('login.html', title='login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
